da2aacb0da23a205574f78f2bbcd69c1eca75eaf
sasmi90 <sasmi90@gmail.com>
2020-10-07 13:37:36 +0530

    repo final

--- src/test/java/sGowda/MavenJava/GitFinalClassMerge.java
+++ src/test/java/sGowda/MavenJava/GitFinalClassMerge.java
@@ -1,5 +1,12 @@
 package sGowda.MavenJava;
 
+import org.junit.Test;
+
 public class GitFinalClassMerge {
 
+	@Test
+	public void finalMessageBeforeMerge()
+	{
+		System.out.println("This is repo user updating one last time");
+	}
 }
